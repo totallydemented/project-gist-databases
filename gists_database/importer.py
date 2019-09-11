b9a4faa4e4078b4e28d376746eea376364eb00c7
totallydemented <code@bsar.me>
2019-09-11 21:45:48 +0000

    completed

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,38 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = 'https://api.github.com/users/{username}/gists'.format(username = username)
+    response = requests.get(url)
+    response.raise_for_status()
+    gists = response.json()
+    
+    for gist in gists:
+    
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url'],
+        }
+
+        db.execute("""
+        INSERT INTO gists (
+            github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url, public, created_at, updated_at, comments, comments_url 
+        ) VALUES ( 
+            :github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, :created_at, :updated_at, :comments, :comments_url
+        );""", params)
+    
+        if commit:
+            db.commit()
+    
+    
+    
+
+    
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,20 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    result  = []
+    sql_query = """SELECT * FROM gists"""
+    if kwargs:
+        if 'github_id' in kwargs:
+            sql_query += ' WHERE github_id == :github_id'
+        elif 'created_at' in kwargs:
+            sql_query += ' WHERE datetime(created_at) == datetime(:created_at)'
+        cursor = db_connection.execute(sql_query, kwargs)
+        gists = cursor.fetchall()
+    else:
+        cursor = db_connection.execute(sql_query)
+        gists = cursor.fetchall()
+    for gist in gists:
+        result.append(Gist(gist))
+    return result
+        
